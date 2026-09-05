e82cd5c798280dd984c2fb9329abf3e0cf68d8fb
harjinder-dev <khalsa.harjinder28@gmail.com>
2026-09-05 20:32:08 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/acme/compensation_api/CompensationApiApplication.java
@@ -0,0 +1,13 @@
+package com.acme.compensation_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompensationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompensationApiApplication.class, args);
+	}
+
+}
